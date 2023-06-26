132e3b3d6a6550f3f515fc6bf09f1ea230f99898
bfpill <max@v3rv.com>
2023-06-26 17:35:17 +1000

    cloudFunction almost there ( I think )
    going to need to go back and changed to signed URL

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,80 +4,103 @@ const os = require('os');
 const path = require('path');
 const fs = require('fs');
 const archiver = require('archiver');
+const { Storage: GCloudStorage } = require('@google-cloud/storage');
 
 admin.initializeApp();
 
-exports.generateFiles = functions.https.onCall(async (data: any, context: any) => {
-  // Ensure the user is authenticated
-  if (!context.auth) {
-    throw new functions.https.HttpsError(
-      'unauthenticated', 
-      'You must be signed in to generate files.'
-    );
-  }
+exports.generateFiles = functions.runWith({ timeoutSeconds: 20 }).https.onCall(async (data: any) => {
 
-  // Generate files from JSON data structure
-  const json = data.json; // This is your JSON data structure
-  
-  createDownloadable(json)
-  const tmpdir = os.tmpdir(); // Temporary directory to store files
-  // TODO: Generate files from JSON and store in tmpdir
+    // Generate files from JSON data structure
+    const json = data.json[0]
 
-  // Zip the files
-  const zipPath = path.join(tmpdir, 'files.zip');
-  const output = fs.createWriteStream(zipPath);
-  const archive = archiver('zip');
-  archive.pipe(output);
-  archive.directory(tmpdir, false);
-  await archive.finalize();
+    const tmpdir = path.join(os.tmpdir(), 'notion-ai-os'); // Create a new directory within the system's temporary directory
 
-  // Upload zip file to Firebase Cloud Storage
-  const storage = new Storage();
-  const bucket = storage.bucket(admin.storage().bucket().name);
-  await bucket.upload(zipPath, {
-    destination: `user_files/${context.auth.uid}/files.zip`,
-  });
+    // Create the directory if it doesn't exist
+    if (!fs.existsSync(tmpdir)) {
+        fs.mkdirSync(tmpdir);
+    }
 
-  // Generate download URL
-  const file = bucket.file(`user_files/${context.auth.uid}/files.zip`);
-  const [url] = await file.getSignedUrl({
-    action: 'read',
-    expires: Date.now() + 1000 * 60 * 60, // 1 hour expiration
-  });
+    createDownloadable(json, tmpdir);
 
-  return { url };
+    console.log("Downloadable created")
+
+    const zipPath = path.join(tmpdir, 'files.zip');
+    const output = fs.createWriteStream(zipPath);
+    const archive = archiver('zip');
+    archive.pipe(output);
+
+    // Listen for the 'entry' event
+    archive.on('entry', (entry: any) => {
+        console.log(`Appended file: ${entry.name}`);
+    });
+
+    // Listen for the 'error' event
+    archive.on('error', (err: any) => {
+        console.error(`Archiver error: ${err}`);
+    });
+
+    archive.directory(tmpdir, false);
+
+    // Wait for the 'end' event before calling archive.finalize()
+    archive.on('end', async () => {
+        console.log('Archiver finished appending files');
+        await archive.finalize();
+        console.log('Archiver finalized');
+    })
+
+    console.log("archiver finalized")
+    const bucket = admin.storage().bucket();
+    await bucket.upload(zipPath, {
+        destination: `user_files/files.zip`,
+    });
+
+
+    console.log("Bucket created")
+    // Generate download URL
+    const file = bucket.file(`user_files/files.zip`);
+    const [url] = await file.getSignedUrl({
+        action: 'read',
+        expires: Date.now() + 1000 * 60 * 60, // 1 hour expiration
+    });
+
+    console.log("Url made: " + url)
+
+    return { url };
 });
 
-interface Node {
+interface PageNode {
     name: string;
     id: string;
     type: string;
-    children?: Node[];
+    children?: PageNode[];
     content?: string;
 }
 
-const createFilesAndFolders = (node: Node, currentPath: string = '.') => {
-    const newPath = path.join(currentPath, node.name);
+const createFilesAndFolders = (node: PageNode, currentPath: string = '.') => {
 
-    if (node.type === 'folder') {
-        if (!fs.existsSync(newPath)) {
-            fs.mkdirSync(newPath);
-        }
+    if (node !== undefined) {
+        console.log("Node Name: " + node.name)
+        console.log("Current Path: " + currentPath)
+        const newPath = path.join(currentPath, node.name);
 
-        if (node.children) {
-            for (const child of node.children) {
-                createFilesAndFolders(child, newPath);
+        if (node.type === 'folder') {
+            if (!fs.existsSync(newPath)) {
+                fs.mkdirSync(newPath);
             }
+
+            if (node.children) {
+                for (const child of node.children) {
+                    createFilesAndFolders(child, newPath);
+                }
+            }
+        } else if (node.type === 'javascript') {
+            fs.writeFileSync(newPath, node.content || '');
         }
-    } else if (node.type === 'javascript') {
-        fs.writeFileSync(newPath, node.content || '');
     }
 };
 
-function createDownloadable (jsonTree: any) { 
-
-    for (const page of jsonTree) {
-        createFilesAndFolders(page);
-    }
-
+function createDownloadable(jsonTree: any, currentPath: string) {
+    console.log("TREE: ")
+    console.log(jsonTree)
+    createFilesAndFolders(jsonTree, currentPath)
 }
